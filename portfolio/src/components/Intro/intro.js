3f8512d64e9c09aedd85d0427b2bb2e3281f868e
Amitesh <amiteshvashishth@gmail.com>
2023-09-08 12:59:40 +0530

    modified intro.js and intro.css

--- portfolio/src/components/Intro/intro.js
+++ portfolio/src/components/Intro/intro.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './intro.css'
+import bg from '../../assets/image.png'
+import btnImg from '../../assets/hireme.png'
+import {Link} from 'react-scroll';
 
 const Intro = () => {
   return (
-    <div>intro</div>
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">I'm <span className="introName">Amitesh</span> <br/>Website Designer</span>
+        <p className="introPara">I'm a skilled web designer with experience in creating <br/> visually appealing and user friendly websites.</p>
+        <Link><button className="btn"><img src={btnImg} alt="" className='btnImg' />Hire me</button></Link>
+      </div>
+      <img src={bg} alt="profile" className="bg" />
+    </section>
   )
 }
 
